8f3478ab9e3ada63c57e8ebc4dcbfe4aff949f7f
duhang <840261937@qq.com>
2018-09-25 18:02:16 +0800

    环境模式

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,19 @@
-// const path = require('path');
-//
-// module.exports = {
-//     configureWebpack: {
-//         entry: './src/lib/index.js',
-//         output: {
-//             filename: 'vue-mini-toast.js',  // 打包生成文件的名字
-//             library: 'vue-mini-toast',      // reqire引入的名字
-//             libraryTarget: 'umd',
-//             umdNamedDefine: true
-//         },
-//     },
-//     css: {extract: false}
-// };
+const path = require('path');
+
+let config = {};
+if (process.env.NODE_ENV === 'production') {
+    config = {
+        configureWebpack: {
+            entry: './src/lib/index.js',
+            output: {
+                filename: 'vue-mini-toast.js',  // 打包生成文件的名字
+                library: 'vue-mini-toast',      // reqire引入的名字
+                libraryTarget: 'umd',
+                umdNamedDefine: true
+            },
+        },
+        css: {extract: false}
+    };
+}
+
+module.exports = config;
